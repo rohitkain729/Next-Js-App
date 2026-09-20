ffcb6f962b4194807583f5ebebbb22efd0b4e681
rohitkain729 <134476482+rohitkain729@users.noreply.github.com>
2026-09-20 06:29:29 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h4>Hi,Rohit</h4>
+    </div>
+  );
+}
